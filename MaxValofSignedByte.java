9c6669642eeda59853d0ec8b46c09015077f0764
S-quad <62528972+Sanjana-quad@users.noreply.github.com>
2024-09-30 17:22:08 +0530

    Add files via upload

---
+++ MaxValofSignedByte.java
@@ -0,0 +1,10 @@
+package assign2;
+
+public class MaxValofSignedByte {
+    public static void main(String[] args) {
+        byte number =125;
+        System.out.println("number: "+number);
+        number= 127;
+        System.out.println("largest value number can store: "+number);
+    }
+}
---
+++ boolean_result.java
@@ -0,0 +1,17 @@
+package assign2;
+
+import java.util.Scanner;
+
+public class boolean_result {
+
+    public static void main(String[] args) {
+        System.out.println("Enter value of a: ");
+        Scanner a = new Scanner(System.in);
+        int x=a.nextInt();
+        System.out.println("Enter value of b: ");
+        a = new Scanner(System.in);
+        int y=a.nextInt();
+        boolean result = x>y;
+        System.out.println("The result is: "+result);
+    }
+}
