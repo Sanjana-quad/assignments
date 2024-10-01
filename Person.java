cb846ce91c6a6af52aa13e4d6c00f9c67098a614
S-quad <62528972+Sanjana-quad@users.noreply.github.com>
2024-10-01 19:16:22 +0530

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,29 +1,41 @@
 package assign2;
 
 
-import java.awt.*;
 import java.time.LocalDate;
 import java.time.Period;
-import java.time.format.DateTimeFormatter;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Person {
     private String first_name;
     private String last_name;
     private String dob;
+    private Period age;
 
     public Person(String first_name, String last_name, String dob) {
         this.first_name = first_name;
         this.last_name = last_name;
-        System.out.println("success: " + dob);
+        this.dob = dob;
+        if(getDob()!=null){
+            String[] dob_part = getDob().split("-");
+            int year = Integer.parseInt(dob_part[0]);
+            int month = Integer.parseInt(dob_part[1]);
+            int date = Integer.parseInt(dob_part[2]);
+            age = Period.between(LocalDate.of(year, month, date), LocalDate.now());
+
+        }
+        else{
+            System.out.println("dob not provided");
+        }
+    }
+    public String getDob() {
+        return dob;
+    }
+    public void setDob(String dob) {
         this.dob = dob;
     }
-    String[] dob_part=getDob().split("-");
-    int year=Integer.parseInt(dob_part[0]);
-    int month=Integer.parseInt(dob_part[1]);
-    int date=Integer.parseInt(dob_part[2]);
-    Period age= Period.between(LocalDate.of(year,month,date),LocalDate.now());
+
+
+
     public String getFirst_name() {
         return first_name;
     }
@@ -36,13 +48,7 @@ public class Person {
     public void setLast_name(String last_name) {
         this.last_name = last_name;
     }
-    public String getDob() {
-        System.out.println("success: "+ dob);
-        return dob;
-    }
-    public void setDob(String dob) {
-        this.dob = dob;
-    }
+
 
 
     public int getAge() {
@@ -65,9 +71,7 @@ public class Person {
         System.out.println("Enter dob:");
         sc = new Scanner(System.in);
         String c = sc.nextLine();
-        System.out.println("Enter c:"+c);
         Person p = new Person(a, b, c);
-        System.out.println("Enter c afteer:"+c);
         p.display();
         sc.close();
     }
